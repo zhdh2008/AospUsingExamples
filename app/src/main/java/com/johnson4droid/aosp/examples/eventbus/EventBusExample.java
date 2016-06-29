9994ba65a727391ba75181f10e65982aed8c5195
zhdh2008 <zhdh2008@gmail.com>
2016-06-29 11:39:45 +0800

    [Added] Add Example for 'EventBus'

---
+++ app/src/main/java/com/johnson4droid/aosp/examples/eventbus/EventBusExample.java
@@ -0,0 +1,52 @@
+package com.johnson4droid.aosp.examples.eventbus;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+/**
+ * Created by zhoudaihui on 2016/6/29.
+ */
+public class EventBusExample {
+    /**
+     * 1. For using EventBus , you should first add the gradle dependency to the file 'build.gradle'
+     * compile 'org.greenrobot:eventbus:3.0.0'
+     */
+
+    /**
+     * 2. Define events {@linkplain MessageEvent}
+     */
+
+    /***
+     * 3. Prepare subscribers
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        // TODO: 2016/6/29 handle the event
+    }
+
+    /**
+     * This method can {@linkplain Activity#onStart()} or {@linkplain Fragment#onStart()}
+     */
+    public void onStart() {
+        EventBus.getDefault().register(this);
+    }
+
+    /**
+     * This method can be {@linkplain Activity#onStop()} or {@linkplain Fragment#onStop()}
+     */
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+    }
+
+    /***
+     * 4. post event
+     */
+    public void postEvent() {
+        EventBus.getDefault().post(new MessageEvent("Hello,everyone !"));
+    }
+
+}
---
+++ app/src/main/java/com/johnson4droid/aosp/examples/eventbus/MessageEvent.java
@@ -0,0 +1,12 @@
+package com.johnson4droid.aosp.examples.eventbus;
+
+/**
+ * Created by zhoudaihui on 2016/6/29.
+ */
+public class MessageEvent {
+    private final String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+}
