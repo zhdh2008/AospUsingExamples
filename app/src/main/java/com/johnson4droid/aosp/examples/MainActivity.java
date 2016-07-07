0b78d04eff813900055f273a095dc21fecfc4f55
zhdh2008 <zhdh2008@gmail.com>
2016-07-07 10:24:37 +0800

    [Added]  Add example for 'universal-image-loader'

--- app/src/main/java/com/johnson4droid/aosp/examples/MainActivity.java
+++ app/src/main/java/com/johnson4droid/aosp/examples/MainActivity.java
@@ -2,6 +2,7 @@ package com.johnson4droid.aosp.examples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = null;
+        tv.setText("xxxx");
     }
 }
---
+++ app/src/main/java/com/johnson4droid/aosp/examples/uil/UILExample.java
@@ -0,0 +1,24 @@
+package com.johnson4droid.aosp.examples.uil;
+
+/**
+ * This class show an example of <a href="https://github.com/nostra13/Android-Universal-Image-Loader">Android-Universal-Image-Loader</a>
+ * Created by zhoudaihui on 2016/7/6.
+ */
+public class UILExample {
+
+    /***
+     * Acceptable URIs Example
+     "http://site.com/image.png" // from Web
+     "file:///mnt/sdcard/image.png" // from SD card
+     "file:///mnt/sdcard/video.mp4" // from SD card (video thumbnail)
+     "content://media/external/images/media/13" // from content provider
+     "content://media/external/video/media/13" // from content provider (video thumbnail)
+     "assets://image.png" // from assets
+     "drawable://" + R.drawable.img // from drawables (non-9patch images)
+     */
+
+
+
+}
+
+
