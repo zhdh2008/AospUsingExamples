40021ba9a6432d6158ea6e80e00c6b458e6a1b0d
zhdh2008 <zhdh2008@gmail.com>
2016-07-18 16:06:05 +0800

    [Modified]  modify the  example for  'Retrofit'

--- app/src/main/java/com/johnson4droid/aosp/examples/retrofit/RetrofitExample.java
+++ app/src/main/java/com/johnson4droid/aosp/examples/retrofit/RetrofitExample.java
@@ -1,9 +1,28 @@
 package com.johnson4droid.aosp.examples.retrofit;
 
+import android.os.Message;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
 /**
  * Created by zhoudaihui on 2016/7/15.
  */
 public class RetrofitExample {
 
+    public static void test() {
+        // TODO: 2016/7/15
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .build();
 
+        GitHubService service = retrofit.create(GitHubService.class);
+        Call<List<Repo>> repos = service.listRepos("octocat");
+
+        Message.obtain();
+
+
+    }
 }
